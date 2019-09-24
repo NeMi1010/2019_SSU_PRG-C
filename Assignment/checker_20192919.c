ba042fcee4cbbf28c602f4117094ad5e9ef0546d
NeMi1010 <tmdals757@gmail.com>
2019-09-24 11:15:17 +0900

    Assignment#1 Uploaded

---
+++ Assignment/checker_20192919.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void) {
+
+	int num, num_a, num_b;
+
+	printf("숫자를 입력하시오 : ");
+	scanf("%d", &num);
+	
+	while (num >= 10) {
+		num_a = num / 10;
+		num_b = num % 10;
+		num = num_a - (num_b * 2);
+	}
+	
+	if(num == 7) printf("7의 배수입니다.\n");
+	else printf("7의 배수가 아닙니다.\n");
+	printf("%d\n", num);
+
+	return 0;
+}
