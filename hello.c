ce4495a0b1c18c5b2514a9c7d54b101b5e26e15b
NeMi1010 <tmdals757@gmail.com>
2019-09-24 14:33:14 +0900

    Useless Source Code Deleted

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(void) {
-	printf("Hello World!\n");
-	printf("It's a 2nd Test\n");
-	return 0;
-}
--- test.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(void) {
-	print("Why?\n");
-	return 0;
-}
